0c54c47b9f5684dc5b60be0443491c2917e02f0a
Maverick Tesini <mavericktesini@gmail.com>
2018-08-03 01:30:25 +0000

    automatically backed up by learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,5 +1,8 @@
 class SongsController < ApplicationController
   def index
+    rescue ActiveRecord::RecordNotFound
+    flash[:notice] = "Wrong song id"
+    redirect_to artists_path
     if params[:artist]#if we are accessing all songs via artist routing
       #we want @songs = artist.songs (considering we access via songs: .artist_id vs artist.id)
       @songs = Artist.find_by(params[:artist_id]).songs
