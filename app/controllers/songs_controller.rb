ef3a54ea8b3f8c2b5e153fa1c2201f09dc3026ab
Maverick Tesini <mavericktesini@gmail.com>
2018-08-03 01:32:33 +0000

    automatically backed up by learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -6,13 +6,13 @@ class SongsController < ApplicationController
     else 
       @songs = Song.all
     end 
-    rescue ActiveRecord::RecordNotFound
-      flash[:notice] = "Wrong song id"
-      redirect_to artists_path
   end
 
   def show
     @song = Song.find(params[:id])
+  rescue ActiveRecord::RecordNotFound
+    flash[:notice] = "Wrong song id"
+    redirect_to artists_path
   end
 
   def new
