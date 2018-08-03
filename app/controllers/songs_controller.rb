eab9df59bd2b9f641fead4ece27b7fb6ae0651f9
Maverick Tesini <mavericktesini@gmail.com>
2018-08-03 01:53:07 +0000

    automatically backed up by learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -2,7 +2,7 @@ class SongsController < ApplicationController
   def index
     if params[:artist]#if we are accessing all songs via artist routing
       #we want @songs = artist.songs (considering we access via songs: .artist_id vs artist.id)
-      @songs = Artist.find_by(params[:artist_id]).songs
+      @songs = Artist.find_by(params[:artist_id]).songs if Artist.find(params[:artist_id]) 
     else 
       @songs = Song.all
     end 
